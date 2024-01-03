7829807e9ad0c1a24ffe25cd7175582781cc497a
Angelina Malmros <linaemalmros@gmail.com>
2024-01-03 15:02:21 +0100

    wip fetching and mapping response

--- src/components/BusCard.tsx
+++ src/components/BusCard.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { mockBuses } from "../utils/mock-buses";
 
+interface IBus {
+    busLine: number,
+    stops: string[]
+}
+
 const BusCard = () => {
+    const [busLines, setBusLines] = useState<IBus[]>([]);
+
+
   return (
     <>
-      {mockBuses.map((bus, index) => (
+      {busLines.map((bus, index) => (
         <div key={index} className="d-inline-flex p-4">
           <h4>Buss {bus.busLine}</h4>
           <ul>
