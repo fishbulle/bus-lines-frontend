844728f99f43d871e53627c33cbaad9df6c4da9f
Angelina Malmros <linaemalmros@gmail.com>
2024-01-03 13:27:52 +0100

    created mock buses

---
+++ src/utils/mock-buses.ts
@@ -0,0 +1,102 @@
+export const mockBuses = [
+  {
+    busLine: 1,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 2,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 3,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 4,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 5,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 6,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 7,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 8,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 9,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+  {
+    busLine: 10,
+    stops: [
+      "Hållplats 1",
+      "Hållplats 2",
+      "Hållplats 3",
+      "Hållplats 4",
+      "Hållplats 5",
+    ],
+  },
+];
