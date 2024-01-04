72b3f8208d8a5b293f2b076d3569ae735a199154
Angelina Malmros <linaemalmros@gmail.com>
2024-01-04 10:36:29 +0100

    Styling

--- src/components/BusView.tsx
+++ src/components/BusView.tsx
@@ -4,7 +4,7 @@ const BusView = () => {
   return (
     <>
       <div>
-        <h1>Topp 10 bussar med flest stopp i Stockholm</h1>
+        <h1 className="mt-4">Topp 10 bussar med flest stopp i Stockholm</h1>
         <p className="fst-italic">
           Hämtat från SLs API Hållplatser och linjer 2
         </p>
