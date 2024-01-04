e4135bb60e62b22c8b97d181db0a11539b2719bd
Angelina Malmros <linaemalmros@gmail.com>
2024-01-04 14:09:45 +0100

    moved buscard component into div

--- src/components/BusView.tsx
+++ src/components/BusView.tsx
@@ -3,13 +3,13 @@ import BusCard from "./BusCard";
 const BusView = () => {
   return (
     <>
-      <div>
-        <h1 className="mt-4">Topp 10 bussar med flest stopp i Stockholm</h1>
+      <div className="mt-5">
+        <h1>Topp 10 bussar med flest stopp i Stockholm</h1>
         <h6 className="fst-italic">
           Hämtat från SLs API Hållplatser och linjer 2
         </h6>
+        <BusCard />
       </div>
-      <BusCard />
     </>
   );
 };
