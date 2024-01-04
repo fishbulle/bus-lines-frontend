7f4191f18acaa7b2920115cf2101595981dfcd02
Angelina Malmros <linaemalmros@gmail.com>
2024-01-04 13:19:23 +0100

    tweaked modal to be able to map stops

--- src/BusModal.tsx
+++ src/BusModal.tsx
@@ -2,19 +2,28 @@ import { Modal } from "react-bootstrap";
 
 interface IModal {
   title: string | number;
-  body: string;
+  body: string[];
+  show: boolean;
+  onHide: () => void;
 }
 
-const BusModal = ({ title, body }: IModal) => {
+const BusModal = ({ title, body, show, onHide }: IModal) => {
   return (
     <>
-      <Modal>
-        <Modal.Dialog>
-          <Modal.Header closeButton>
-            <Modal.Title>{title}</Modal.Title>
-          </Modal.Header>
-          <Modal.Body><li>{body}</li></Modal.Body>
-        </Modal.Dialog>
+      <Modal show={show} onHide={onHide} centered>
+        <Modal.Header closeButton closeVariant="white" className="border-0 px-4 bg-dark">
+          <Modal.Title className="fw-bold">{title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="border-0 bg-dark">
+          <p className="px-2">Hållplatser:</p>
+          <div style={{columns: "2 auto"}}>
+          {body.map((item, index) => (
+            <ul>
+              <li key={index}>{item}</li>
+            </ul>
+          ))}
+          </div>
+        </Modal.Body>
       </Modal>
     </>
   );
